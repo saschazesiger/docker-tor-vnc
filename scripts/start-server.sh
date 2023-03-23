f8564d3de7e021183663d180f487808811b1ace5
Janis <76683226+saschazesiger@users.noreply.github.com>
2023-03-23 10:40:54 +0000

    run as root

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -45,5 +45,6 @@ websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem ${NOVNC_PORT} lo
 sleep 2
 
 echo "---Starting Tor-Browser---"
+sudo su
 chmod +x /tor-browser/start-tor-browser.desktop
 /tor-browser/start-tor-browser.desktop --display=:99 --P ${USER} --setDefaultBrowser ${EXTRA_PARAMETERS}
