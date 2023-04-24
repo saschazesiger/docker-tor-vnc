2783a8b9838f913822f0050566ae58d447abfdf6
Janis <76683226+saschazesiger@users.noreply.github.com>
2023-04-24 22:10:15 +0000

    Update

---
+++ scripts/start-container.sh
@@ -0,0 +1,11 @@
+wget -q -nc --show-progress --progress=bar:force:noscroll -O /torbrowser/Tor-Browser.tar.xz "https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-12.0.5/tor-browser-linux64-12.0.5_ALL.tar.xz" ; then
+echo "---Sucessfully downloaded Tor-Browser---"
+
+tar -C /torbrowser --strip-components=2 -xf /torbrowser/Tor-Browser.tar.xz
+rm -f /torbrowser/Tor-Browser.tar.xz
+
+cp -R /tmp/profile/Browser /torbrowser/TorBrowser/Data/
+rm -f /torbrowser/Tor-Browser.tar.xz
+rm -rf /tmp/profile
+
+
