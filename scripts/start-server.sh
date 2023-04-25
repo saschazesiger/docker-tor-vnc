6f573ccb966281e0b9a17a5467c32bab0a2b607c
Janis <76683226+saschazesiger@users.noreply.github.com>
2023-04-25 16:49:44 +0000

    removed variables in server

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,32 +2,15 @@
 export DISPLAY=:99
 export XAUTHORITY=/torbrowser/.Xauthority
 
-echo "---Resolution check---"
-if [ -z "${CUSTOM_RES_W} ]; then
-	CUSTOM_RES_W=1024
-fi
-if [ -z "${CUSTOM_RES_H} ]; then
-	CUSTOM_RES_H=768
-fi
-
-if [ "${CUSTOM_RES_W}" -le 1023 ]; then
-	echo "---Width to low must be a minimal of 1024 pixels, correcting to 1024...---"
-    CUSTOM_RES_W=1024
-fi
-if [ "${CUSTOM_RES_H}" -le 767 ]; then
-	echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
-    CUSTOM_RES_H=768
-fi
-
 
 screen -wipe 2&>/dev/null
 
 echo "---Starting TurboVNC server---"
-vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
-sleep 2
+vncserver -geometry 1024x768 -depth ${CUSTOM_DEPTH} :99 -rfbport 5900 -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
+
 echo "---Starting Fluxbox---"
 /opt/scripts/start-fluxbox.sh &
-sleep 2
+
 
 
 echo "---Starting Tor-Browser---"
